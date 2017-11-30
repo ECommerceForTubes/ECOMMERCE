941c0b20a2164be6368fc5527a46a37af440c561
DIMASS27 <dimas.prasetyo.aji027@gmail.com>
2017-11-30 21:24:47 +0700

    perubahan di transaksi

--- ~Proyek Besar Tubes PBO~/~Proyek Besar Tubes PBO~/TUBES Net/T_Ne_tV0.1Apple/src/model/Customer.java
+++ ~Proyek Besar Tubes PBO~/~Proyek Besar Tubes PBO~/TUBES Net/T_Ne_tV0.1Apple/src/model/Customer.java
@@ -5,11 +5,30 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Dimas
  */
-public class Customer {
+public class Customer extends Account{
     private String id_customer;
-   
+    private List<Transaction> transactions= new ArrayList<>();
+    private List<Donation> donations= new ArrayList<>();
+
+    public Customer(String id_customer, String name, String password, String address, String id_card, String e_mail) {
+        super(name, password, address, id_card, e_mail);
+        this.id_customer = id_customer;
+    }
+    
+    public void addDonation(String name, String type_donation, String discription, String date, int count_donation){
+       Donation donation = new Donation(name, type_donation, discription, date, count_donation);
+       donations.add(donation);
+    }
+    
+    public void addTransaction(String code_transaction, Payment payment){
+       Transaction transaction = new Transaction(code_transaction, payment);
+       transactions.add(transaction);
+    }
 }
