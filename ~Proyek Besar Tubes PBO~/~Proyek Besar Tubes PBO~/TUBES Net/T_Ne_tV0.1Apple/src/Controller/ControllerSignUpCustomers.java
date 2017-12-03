aff89ac32edb92664ed863817cf15e896916d913
DIMASS27 <dimas.prasetyo.aji027@gmail.com>
2017-12-03 15:39:43 +0700

    dimas

---
+++ ~Proyek Besar Tubes PBO~/~Proyek Besar Tubes PBO~/TUBES Net/T_Ne_tV0.1Apple/src/Controller/ControllerSignUpCustomers.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import View.SignUpCustomer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Dimas
+ */
+public class ControllerSignUpCustomers implements ActionListener{
+    SignUpCustomer signUpCustomer;
+    public ControllerSignUpCustomers() {
+        signUpCustomer = new SignUpCustomer();
+        signUpCustomer.setVisible(true);
+        signUpCustomer.setLocationRelativeTo(null);
+        signUpCustomer.addlistener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object a = e.getSource();
+        if (a == signUpCustomer.getSignUp()) {
+            String user = signUpCustomer.getRegName().getText();
+            String pass = signUpCustomer.getRegPassword().getText();
+            String email = signUpCustomer.getRegemail().getText();
+            String addrs = signUpCustomer.getRegAddress().getText();
+            String idnum = signUpCustomer.getRegIdnum().getText();
+            System.out.println(user + pass);
+        }
+    }
+}
